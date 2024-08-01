bb5c3273abed1f028fe29216a9e47d923395e6f1
Joowon Lim <m4rinesnow34@gmail.com>
2024-08-02 08:17:26 +0900

    Fix: 미출석시 퇴실 안함

--- content.js
+++ content.js
@@ -1,6 +1,9 @@
 let checkInElement = document.querySelector("#checkIn");
 let checkOutElement = document.querySelector("#checkOut");
 
+const CHECK_IN_TEXT = "입실하기"
+const CHECK_OUT_TEXT = "퇴실하기"
+
 function nowHour() {
   let now = new Date();
   return now.getHours();
@@ -12,8 +15,7 @@ function nowMinute(){
 }
 
 if (checkInElement) {
-  if ((nowHour() >= 8 && nowMinute() >= 30) && checkInElement.textContent.includes("입실")) {
-    console.log("클릭");
+  if ((nowHour() >= 8 && nowMinute() >= 30) && checkInElement.textContent.includes(CHECK_IN_TEXT)) {
     checkInElement.click();
     setTimeout(function() {
       location.reload(true);
@@ -22,10 +24,8 @@ if (checkInElement) {
     console.log(`현재 시간 ${nowHour()} : ${nowMinute()}은 입실 시간이 아닙니다.` );
   }
 } 
-
-if (checkOutElement) {
-  if ((nowHour() == 18 && nowMinute <= 30)&& checkOutElement.textContent.includes("퇴실하기")) {
-    console.log("클릭");
+else if (checkOutElement) {
+  if ((nowHour() == 18 && nowMinute <= 30)&& checkOutElement.textContent.includes(CHECK_OUT_TEXT)) {
     checkOutElement.click();
     setTimeout(function() {
       location.reload(true);
