e6947805fd684596d9042c5ec814b95bb1f84622
Joowon Lim <m4rinesnow34@gmail.com>
2024-08-02 08:00:55 +0900

    Feat: 시간 판단 구현

--- content.js
+++ content.js
@@ -1,15 +1,36 @@
-// content.js
-let selector = "#checkIn";
-let element = document.querySelector(selector);
+let checkInElement = document.querySelector("#checkIn");
+let checkOutElement = document.querySelector("#checkOut");
 
-if (element) {
-  if (element.textContent.includes("입실")) {
+function nowHour() {
+  let now = new Date();
+  return now.getHours();
+}
+
+function nowMinute(){
+  let now = new Date();
+  return now.getMinutes();
+}
+
+if (checkInElement) {
+  if ((nowHour() >= 8 && nowMinute() >= 30) && checkInElement.textContent.includes("입실")) {
     console.log("클릭");
-    element.click();
+    checkInElement.click();
+    setTimeout(function() {
+      location.reload(true);
+    }, 1000);    
   } else {
-    console.log("입실 없음");
+    console.log(`현재 시간 ${nowHour()} : ${nowMinute()}은 입실 시간이 아닙니다.` );
   }
 } 
-else{
-  console.log("element 없음");
-}
+
+if (checkOutElement) {
+  if ((nowHour() >= 20 && nowMinute <= 30)&& checkOutElement.textContent.includes("퇴실하기")) {
+    console.log("클릭");
+    checkOutElement.click();
+    setTimeout(function() {
+      location.reload(true);
+    }, 1000);    
+  } else {
+    console.log(`현재 시간 ${nowHour()} : ${nowMinute()}은 퇴실 시간이 아닙니다.` );
+  }
+} 
